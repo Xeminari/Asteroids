5d7ac72cab133b195e6cfc8130d0af445410d749
Xeminari <kingikingi66@gmail.com>
2025-01-29 12:06:38 +0100

    Asteroids: circleshape.py, player.py, constants.py updated, main.py (fps, player shape, player movement)

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame
-from constants import *
+import pygame                                                               # import pygame
+from constants import *                                                     # import from other py file
+from player import *                                         
 
 def main():
-    pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    pygame.init()                                                           #initialize pygame
+    clock = pygame.time.Clock()                                             # Clock object
+    dt = 0                                                                  # delta time
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))         # screen resolution imported from constants
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    while True:                                                             # inf while loop
+        for event in pygame.event.get():                                    
+            if event.type == pygame.QUIT:                                   # when x'ing game python code stops
                 return
-        screen.fill((0,0,0))
-        pygame.display.flip()
-
+        
+        screen.fill((0,0,0))                                                # black color
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()                                               # idk
+        dt = clock.tick(60)/1000                                            # frame limit
+        
 
 
 
